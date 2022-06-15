a6bdf99b8d8b8cefcdd532dd3a4d55db48276f41
Fauziah Aulia <fauziahaulia21@gmail.com>
2022-06-15 08:39:39 +0700

    update

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import Product from '../views/Product.vue'
+import Reset from '../views/Reset.vue'
 
 Vue.use(VueRouter)
 
@@ -17,7 +19,17 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+  },
+  {
+    path: '/product',
+    name: 'product',
+    component: Product
+  },
+  {
+    path: '/reset',
+    name: 'reset',
+    component: Reset
+  },
 ]
 
 const router = new VueRouter({
---
+++ src/views/Reset.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="reset">
+        <!-- navbar -->
+    <div class="container">
+        <nav class="row navbar navbar-expand-lg navbar-light bg-white">
+            <div class="navbar-nav ml-auto mr-auto mr-sm-auto mr-lg-0 mr-md-auto">
+                <a href="#" class="navbar-brand">
+                    <img class="logo" src="frontend/images/logo.png" alt="logo" />
+                </a>
+            </div>
+            <ul class="navbar-nav mr-auto d-none d-lg-block">
+                <li>
+                    <span class="text-muted">
+                        | &nbsp; Find The Best Fit
+                    </span>
+                </li>
+            </ul>
+        </nav>
+    </div>
+
+    <main>
+        <section class="section-verify d-flex align-items-center">
+            <div class="col text-center">
+                <img class="forgot" src="frontend/images/reset-password.jpg" alt="">
+                <h1>Please Check Your Email to Reset Your Password</h1>
+            </div>
+        </section>
+    </main>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "ResetPasswordView",
+}
+</script>
+
+<style scoped>
+*{
+    background-color: #fff;
+}
+</style>
